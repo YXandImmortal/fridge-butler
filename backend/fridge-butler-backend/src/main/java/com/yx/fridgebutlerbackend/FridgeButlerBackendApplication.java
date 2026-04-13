92d027264689aaebe8735cccc002b664af98e27c
YXandImmortal <2830162806@qq.com>
2026-04-13 15:37:00 +0800

    后端项目搭建

---
+++ backend/fridge-butler-backend/src/main/java/com/yx/fridgebutlerbackend/FridgeButlerBackendApplication.java
@@ -0,0 +1,13 @@
+package com.yx.fridgebutlerbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FridgeButlerBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FridgeButlerBackendApplication.class, args);
+    }
+
+}
