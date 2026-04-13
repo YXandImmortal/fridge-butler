e92bb998ea4c3c6ee6f6f8ef7056c8b80fcc6b8d
YXandImmortal <2830162806@qq.com>
2026-04-13 16:08:18 +0800

    创建实体类SysUser，SysRole

---
+++ backend/fridge-butler-backend/src/main/java/com/yx/fridgebutler/entity/SysUser.java
@@ -0,0 +1,54 @@
+package com.yx.fridgebutler.entity;
+
+import jakarta.persistence.*;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Size;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import org.hibernate.annotations.ColumnDefault;
+
+import java.time.Instant;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+@Setter
+@Entity
+@Table(name = "sys_user")
+public class SysUser {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id", nullable = false)
+    private Long id;
+
+    @Size(max = 50)
+    @NotNull
+    @Column(name = "username", nullable = false, length = 50)
+    private String username;
+
+    @Size(max = 60)
+    @NotNull
+    @Column(name = "password", nullable = false, length = 60)
+    private String password;
+
+    @NotNull
+    @Column(name = "role_id", nullable = false)
+    private Long roleId;
+
+    @NotNull
+    @ColumnDefault("CURRENT_TIMESTAMP")
+    @Column(name = "create_time", nullable = false)
+    private Instant createTime;
+
+    @ColumnDefault("CURRENT_TIMESTAMP")
+    @Column(name = "update_time")
+    private Instant updateTime;
+
+    @ColumnDefault("0")
+    @Column(name = "is_deleted")
+    private Boolean isDeleted;
+
+
+}
