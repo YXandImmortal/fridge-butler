9a40caef09b38fa9208eba504de24e6bcaf1d303
YXandImmortal <2830162806@qq.com>
2026-04-13 16:04:28 +0800

    更改包名

--- backend/fridge-butler-backend/src/main/java/com/yx/fridgebutler/FridgeButlerBackendApplication.java
+++ backend/fridge-butler-backend/src/main/java/com/yx/fridgebutler/FridgeButlerBackendApplication.java
@@ -1,4 +1,4 @@
-package com.yx.fridgebutlerbackend;
+package com.yx.fridgebutler;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
--- backend/fridge-butler-backend/src/test/java/com/yx/fridgebutler/FridgeButlerBackendApplicationTests.java
+++ backend/fridge-butler-backend/src/test/java/com/yx/fridgebutler/FridgeButlerBackendApplicationTests.java
@@ -1,4 +1,4 @@
-package com.yx.fridgebutlerbackend;
+package com.yx.fridgebutler;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
