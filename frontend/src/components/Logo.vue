a84481511c53b14d62fd33cf8e2b2a0d4d8718fe
YXandImmortal <2830162806@qq.com>
2026-04-18 17:16:45 +0800

    新增了系统Logo，使用SVG格式，创建了对应的组件

---
+++ frontend/src/components/Logo.vue
@@ -0,0 +1,15 @@
+<template>
+  <img src="@/assets/Logo.svg" alt="冰箱管理系统" class="logo-svg" />
+</template>
+
+<script setup>
+// 组件逻辑
+</script>
+
+<style scoped>
+.logo-svg {
+  width: 100%;
+  height: 100%;
+  object-fit: contain;
+}
+</style>
