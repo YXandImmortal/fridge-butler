10f412c6d173a919ea8aaf0d29a3339e97263019
YXandImmortal <2830162806@qq.com>
2026-05-07 08:56:58 +0800

    新增了更新物品请求与去除物品请求

---
+++ backend/fridge-butler-backend/src/main/java/com/yx/fridgebutler/dto/ItemTakeOutRequest.java
@@ -0,0 +1,29 @@
+package com.yx.fridgebutler.dto;
+
+import jakarta.validation.constraints.DecimalMin;
+import jakarta.validation.constraints.NotNull;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.math.BigDecimal;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class ItemTakeOutRequest {
+
+    /**
+     * 物品ID
+     */
+    @NotNull(message = "物品ID不能为空")
+    private Long id;
+
+    /**
+     * 取出数量
+     */
+    @NotNull(message = "取出数量不能为空")
+    @DecimalMin(value = "0.01", message = "取出数量必须大于0")
+    private BigDecimal takeOutNum;
+
+}
